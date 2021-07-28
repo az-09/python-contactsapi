c7bf2dc406bca66bc6204d194fb980f656aee9ce
taehee <dadac76@hotmail.com>
2021-07-28 10:27:29 -0700

    stasticfiles in settings

--- contactsapi/settings.py
+++ contactsapi/settings.py
@@ -10,13 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+
 import django_heroku
 from dotenv import load_dotenv, find_dotenv
 from pathlib import Path
 import os
 load_dotenv(find_dotenv())
-
-django_heroku.setttings(locals())
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -154,3 +153,6 @@ SWAGGER_SETTINGS = {
         }
     }
 }
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+django_heroku.settings(locals())
