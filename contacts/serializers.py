126127e6304ed154622517f301118857157df535
taehee <dadac76@hotmail.com>
2021-08-03 12:33:08 -0700

    __all__ makes id mandatory

--- contacts/serializers.py
+++ contacts/serializers.py
@@ -7,6 +7,6 @@ class ContactSerializer(ModelSerializer):
 
     class Meta:
         model = Contact
-        fields = '__all__'
-        # fields = ['country_code', 'first_name', 'last_name',
-        #           'phone_number', 'picture_url', 'is_favorite']
+        # fields = '__all__'
+        fields = ['country_code', 'first_name', 'last_name',
+                  'phone_number', 'picture_url', 'is_favorite']
